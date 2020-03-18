07af2867db22a61b6dc79660a973c7c36b1e7e18
brandonlwu <brandonlwu02@gmail.com>
2020-03-18 14:41:40 -0700

    Delete thanks.js

--- js/thanks.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-		for (let i = 1; i <=22; i++) {
-			var newNode = document.createElement("div");
-			newNode.style.backgroundImage = ("url('profiles/" + i +".jpg')");
-			document.getElementById("grid-thanks").appendChild(newNode);
-		}
-});
