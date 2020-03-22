be128527ac5bb51f794603e591622e40f2b5daf4
brandonlwu <brandonlwu02@gmail.com>
2020-03-21 23:33:54 -0700

    Update thanks.js

--- js/thanks.js
+++ js/thanks.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-		for (let i = 1; i <=44; i++) {
+		for (let i = 1; i <=100; i++) {
 			var newNode = document.createElement("div");
 			newNode.style.backgroundImage = ("url('profiles/" + i +".jpg')");
 			document.getElementById("grid-thanks").appendChild(newNode);
